bd2449b354139ce62ad14503412b1b53b2391bd7
Alex Welch <me@alexwelch.com>
2015-01-16 12:47:58 -0700

    wip on modal shizzle

---
+++ spec/javascripts/jquery.remote_contents_dialog_spec.js
@@ -0,0 +1,44 @@
+describe('$.fn.remoteContentsDialog', function() {
+  var subject,
+      fakeModal;
+
+  beforeEach(function() {
+    fixture.load('remote_contents_modal.html');
+    jasmine.Ajax.useMock();
+    fakeModal = jasmine.createSpyObj('modal', ['dialog'])
+    spyOn($.PMX.Helpers, 'dialog').andReturn(fakeModal);
+    $('body').remoteContentsDialog({ targetSelector: '#modal_launcher' });
+  });
+
+  describe('clicking the trigger', function() {
+    it('makes an ajax request to the URL from the link', function() {
+      $('a#modal_launcher').click();
+
+      var request = mostRecentAjaxRequest();
+      expect(request.url).toEqual('foo.com/bar');
+    });
+
+    it('triggers the modal with the remote contents', function() {
+      $('a#modal_launcher').click();
+
+      var request = mostRecentAjaxRequest();
+      request.response({
+        status: 200,
+        responseText: '<div class="plain-contents">response content</div>'
+      });
+
+      expect($.PMX.Helpers.dialog).toHaveBeenCalledWith(
+        jasmine.any(Object),
+        jasmine.any(jQuery),
+        { title: '' }
+      );
+
+      expect(fakeModal.dialog).toHaveBeenCalledWith('open');
+    });
+
+    describe('handleClose', function() {
+
+    });
+  });
+
+});
